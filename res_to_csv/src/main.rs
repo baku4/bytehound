5151230addcf82defbb61e3ec11b7275de096cce
baku4 <bahkhun@gmail.com>
2022-11-25 18:54:33 +0900

    Fix parsing args

--- res_to_csv/src/main.rs
+++ res_to_csv/src/main.rs
@@ -4,8 +4,12 @@ use bytehound_res_to_csv::{get_data, write_data_to_stdout};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    eprintln!("Bytehound result(.dat) to csv format");
+    eprintln!(" - format: time(ns), size");
+    eprintln!(" - bin path: {}", args[0]);
+    eprintln!(" - input bytehound dat: {}", args[1]);
 
-    let input_file = PathBuf::from(args[0].clone());
+    let input_file = PathBuf::from(args[1].clone());
     let data = get_data(&input_file);
 
     write_data_to_stdout(data);
